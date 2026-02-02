ed1e433361beaaf20ac06c86b90d5c3df97e71f3
Prashant Singh <prshnnt@gmail.com>
2026-02-02 20:10:28 +0530

    cahnged something

---
+++ src/orchestration/state.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+from typing import List , Optional 
+
+class Message(BaseModel):
+    message : str
+class Document(BaseModel):
+    id: str
+    content: str
+    metadata: Optional[dict] = None
+
+class State(BaseModel):
+    user_id : int
+    query : str
+    messages : List[Message]
+    documents :
